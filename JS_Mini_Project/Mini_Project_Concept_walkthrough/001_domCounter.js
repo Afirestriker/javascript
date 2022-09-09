f7e301cbb8d0c42cde6881407b8593c8b0e9f111
Afirestriker <agrawalashish.in@gmail.com>
2022-09-09 19:38:09 +0530

    Added JS interval to 001_domCounter

--- JS_Mini_Project/Mini_Project_Concept_walkthrough/001_domCounter.js
+++ JS_Mini_Project/Mini_Project_Concept_walkthrough/001_domCounter.js
@@ -1,13 +1,3 @@
-
-let count = 0;
-
-function incrementCount() {
-    document.querySelector('#txtCount').innerHTML = ++count; 
-
-    if(count % 10 === 0)
-        alert(`Counter ${count} is a multiple of 10`);
-}
-
 /* NOTE:
   - since in 001_domCounter.html file, we have loaded script inside head tag, which comes before <body>
     we are using 'DOMcontentLoaded' event listener, so that the portion of this JS code, execute
@@ -20,6 +10,16 @@ function incrementCount() {
     functionality/concept.
  */
 document.addEventListener('DOMContentLoaded', () => {
+
+    let count = 0;
+    const txtCount = document.querySelector('#txtCount');
+
+    function incrementCount() {
+        txtCount.innerHTML = ++count; 
+        if(count % 10 === 0)
+            alert(`Counter ${count} is a multiple of 10`);
+    }
+
     document.querySelector('#incrementCount').onclick = incrementCount;
     /* incrementCount is a function, and omitting the () parenthesis means, 
         the incrementCount() function is only going to be call when we click on 
@@ -28,4 +28,16 @@ document.addEventListener('DOMContentLoaded', () => {
         If we had put (), then it will run the function as soon as the statement (line:23)
         has got executed.
     */
+    
+    /* Auto increment count value after every 1 second */
+    let interval_ID = 0;
+    document.querySelector('#startAutoIncrement').onclick = function() {
+        txtCount.style.color = "green";
+        interval_ID = setInterval(incrementCount, 1000)
+    }
+
+    document.querySelector('#stopAutoIncrement').onclick = function() {
+        txtCount.style.color = "red";
+        clearInterval(interval_ID);
+    }
 });
