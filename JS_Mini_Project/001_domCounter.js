59fd879c55d1fa10ec415ff4b745f3f8a1f632ed
Afirestriker <agrawalashish.in@gmail.com>
2022-09-09 16:29:43 +0530

    Updated 001_domCounter HTML and JS

--- JS_Mini_Project/001_domCounter.js
+++ JS_Mini_Project/001_domCounter.js
@@ -1,19 +1,31 @@
 
-const txtCount = document.querySelector('#count');
 let count = 0;
 
 function incrementCount() {
-    txtCount.innerHTML = ++count; 
+    document.querySelector('#txtCount').innerHTML = ++count; 
 
     if(count % 10 === 0)
         alert(`Counter ${count} is a multiple of 10`);
 }
 
-document.querySelector('#incrementCount').onclick = incrementCount;
-/* incrementCount is a function, and omitting the () parenthesis means, 
-    the incrementCount() function is only going to be call when we click on 
-    button with id 'incrementCount'.
+/* NOTE:
+  - since in 001_domCounter.html file, we have loaded script inside head tag, which comes before <body>
+    we are using 'DOMcontentLoaded' event listener, so that the portion of this JS code, execute
+    after the whole html <body> is loaded. SO that the button with id "incrementCount" will be found.
 
-    If we had put (), then it will run the function as soon as the statement (line:23)
-    has got executed.
+    Else, if we could have loaded <script src=""></scritp> after the html <body> tag. 
+    we won't required addEventListener in that scenario.
+
+    Recommended way: load <script> after the body. This is done just for practice and being aware of the 
+    functionality/concept.
+ */
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('#incrementCount').onclick = incrementCount;
+    /* incrementCount is a function, and omitting the () parenthesis means, 
+        the incrementCount() function is only going to be call when we click on 
+        button with id 'incrementCount'.
+    
+        If we had put (), then it will run the function as soon as the statement (line:23)
+        has got executed.
     */
+});
