2d463ed0d010c4c69ac2f768652da464862bd1fc
Afirestriker <agrawalashish.in@gmail.com>
2023-03-06 18:11:20 +0530

    Added CSS animation

--- JS_Mini_Projects/Infinite-Scroll/infiniteScroll.js
+++ JS_Mini_Projects/Infinite-Scroll/infiniteScroll.js
@@ -1,6 +1,6 @@
 /**Counter to keep total count of 'p' element */
 let count = 0;
-let maxLimit = 20;
+const maxLimit = 20;
 /**Variable to store an html element with id='numberSeries' */
 const numberSeries = document.getElementById("numberSeries");
 
@@ -12,13 +12,30 @@ function appendPara() {
     for(let i=0; i<maxLimit; i++) {
         const div = document.createElement("div");
         div.className = "posts";
-        div.innerHTML = `Post #${++count}`;
+        div.innerHTML = `Post #${++count} <button class="hide">Hide</button>`;
+
         numberSeries.appendChild(div);
     }
 }
 
-/**window vertical scroll event handler
- * Add 10 more 'p' elements, if vertical scroll, reaches the end of document
+/**
+ * Hide post element on hide button click.
+ * This function is executed for all click event, irrespective of where you click on the entire document.
+ * But the if statements will only execute if the click is trigger on button with class 'hide'.
+ */
+document.addEventListener('click', eve => {
+    const btnElement = eve.target;
+    if(btnElement.className === 'hide') {
+        btnElement.parentElement.style.animationPlayState = 'running';
+        btnElement.parentElement.addEventListener('animationend', () => {
+            btnElement.parentElement.remove();
+        });
+    }
+})
+
+/**
+ * window vertical scroll event handler
+ * Append 10 more 'p' elements, if vertical scroll reaches the end of document
  */
 window.onscroll = () => {
     if(window.innerHeight + window.scrollY >= document.body.offsetHeight) {
